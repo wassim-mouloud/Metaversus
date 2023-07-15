88c4f04661f897b673886e56a49ec7522100e573
wassim-mouloud <simo.rko10@gmail.com>
2023-07-15 18:53:39 +0100

    fixing explore section bug

--- app/page.js
+++ app/page.js
@@ -3,23 +3,23 @@ import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World }
 
 const Page = () => (
   <div className="">
-  <Navbar />
+ 
   
   <Hero />
   <div className="relative">
     <About />
-    <div className="gradient-03 z-0" />
+    <div className="z-0 gradient-03" />
     <Explore />
   </div>
   <div className="relative">
     <GetStarted />
-    <div className="gradient-04 z-0" />
+    <div className="z-0 gradient-04" />
     <WhatsNew />
   </div>
   <World />
   <div className="relative">
     <Insights />
-    <div className="gradient-04 z-0" />
+    <div className="z-0 gradient-04" />
     <Feedback />
   </div>
   <Footer />
--- constants/index.js
+++ constants/index.js
@@ -2,27 +2,27 @@ export const exploreWorlds = [
   {
     id: 'world-1',
     imgUrl: '/planet-01.png',
-    title: 'The Hogwarts',
+    title: 'Hogwarts',
   },
   {
     id: 'world-2',
     imgUrl: '/planet-02.png',
-    title: 'The Upside Down',
+    title: 'Upside',
   },
   {
     id: 'world-3',
     imgUrl: '/planet-03.png',
-    title: 'Kadirojo Permai',
+    title: 'Permai',
   },
   {
     id: 'world-4',
     imgUrl: '/planet-04.png',
-    title: 'Paradise Island',
+    title: 'Paradise',
   },
   {
     id: 'world-5',
     imgUrl: '/planet-05.png',
-    title: 'Hawkins Labs',
+    title: 'Labs',
   },
 ];
 
