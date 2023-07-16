9da1f294980218293d53435828faf576a8ca51c0
wassim-mouloud <simo.rko10@gmail.com>
2023-07-16 16:24:13 +0100

    adding some stuff before deploying

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -24,15 +24,7 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
       </h3>
     ) : (
       <div className="absolute bottom-0 p-8 flex justify-start w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-[24px]">
-        <div
-          className={`${styles.flexCenter} w-[60px] h-[60px] rounded-[24px] glassmorphism mb-[16px]`}
-        >
-          <img
-            src="/headset.svg"
-            alt="headset"
-            className="object-contain w-1/2 h-1/2"
-          />
-        </div>
+j
         <p className="font-normal text-[16px] leading-[20.16px] text-white uppercase">
           Enter Metaverse
         </p>
